afbd83f6d754c581921115102160783dad377693
Ben Logie <2454767l@student.gla.ac.uk>
2021-02-04 16:41:27 +0000

    Commit before end of Ch 5 tests

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+admin.site.register(Category)
+admin.site.register(Page)
+
 
 # Register your models here.
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('', views.index, name = 'index'),
+    path('', views.index, name = 'index'),
     path('rango/', include('rango.urls')),
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
